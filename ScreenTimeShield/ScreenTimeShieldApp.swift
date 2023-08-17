ee3c055d0346aeff3e1ddce7fdae678cd14f8de5
divo <divoapple@gmail.com>
2023-08-17 14:45:57 +0100

    Add family controls capability and request access
    Also an example of how to call async code from sync code using a Task

--- ScreenTimeShield/ScreenTimeShieldApp.swift
+++ ScreenTimeShield/ScreenTimeShieldApp.swift
@@ -6,12 +6,33 @@
 //
 
 import SwiftUI
+import FamilyControls
 
 @main
 struct ScreenTimeShieldApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  
+  @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    Task {
+      await requestFamilyControls()
+    }
+    return true
+  }
+  
+  func requestFamilyControls() async {
+    do {
+      try await AuthorizationCenter.shared.requestAuthorization(for: .individual)
+    } catch {
+      print("Error requesting Family Controls: \(error)")
+    }
+  }
 }
