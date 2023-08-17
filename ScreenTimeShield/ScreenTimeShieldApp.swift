e7c33fe9f96e2923c02451495f0463705a703e99
divo <divoapple@gmail.com>
2023-08-17 14:17:46 +0100

    Initial Commit

---
+++ ScreenTimeShield/ScreenTimeShieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeShieldApp.swift
+//  ScreenTimeShield
+//
+//  Created by Steven Diviney on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeShieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
